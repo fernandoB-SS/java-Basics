14cf2f30a4969c0d6c58b60805f3423f37538f10
Fenando Becerra <fern>
2021-11-02 09:29:50 -0700

    formatting

--- da1_assignment/day1_assignment/src/main/java/com/ss/week1/day1/day1_assignment/Day1Aassignment2.java
+++ da1_assignment/day1_assignment/src/main/java/com/ss/week1/day1/day1_assignment/Day1Aassignment2.java
@@ -11,18 +11,22 @@ public class Day1Aassignment2 {
     // Function random guess
     public static void randomGuess() {
         // generate random number from 1-100
-        int randInt = ThreadLocalRandom.current().nextInt(1, 6);
+        int randInt = ThreadLocalRandom.current().nextInt(1, 100);
+        // variable declaration
+        int numberOfGuesses = 5;
+        boolean gameOver = false;
+        // random + 10 & random -10 to set range to guess
         int upperLimit = randInt + 10;
         int lowerLimit = randInt - 10;
-        int numberOfGuesses = 5;
+
         // prompt user
-        System.out.println("Range between 1 to 6: " + randInt);
+        System.out.println("Range between 1 to 100: " + randInt);
         // scanner object
         Scanner scanObj = new Scanner(System.in);
         // store user integer guess
         int userGuess = scanObj.nextInt();
-        boolean gameOver = false;
 
+        // while loop
         while (gameOver == false) {
             if (userGuess == randInt) {
                 System.out.println("Congratulations you won!");
@@ -34,15 +38,6 @@ public class Day1Aassignment2 {
             } else if (numberOfGuesses == 0) {
                 System.out.println("Congratulations you won!");
                 gameOver = true;
-
-            } else if (userGuess > lowerLimit && userGuess < upperLimit) {
-                System.out.println("Upper Limit:" + upperLimit + "\nLowerLimit: " + lowerLimit);
-                System.out.println("Congratulations you won!");
-                gameOver = true;
-            } else if (numberOfGuesses == 0) {
-                System.out.println("Game over!");
-                gameOver = true;
-
             } else {
                 numberOfGuesses--;
                 System.out.println("Number of tries: " + numberOfGuesses);
@@ -51,7 +46,6 @@ public class Day1Aassignment2 {
                 userGuess = scanObj.nextInt();
             }
         }
-
         // scanner object close
         scanObj.close();
 
@@ -60,7 +54,6 @@ public class Day1Aassignment2 {
     public static void main(String[] args) {
         SpringApplication.run(Day1AssignmentApplication.class, args);
         // Welcome
-
         System.out.println("Week1 Day");
         // function call
         randomGuess();
