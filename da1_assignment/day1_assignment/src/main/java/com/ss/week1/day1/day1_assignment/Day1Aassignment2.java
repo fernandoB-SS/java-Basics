20aa5b4a6072a257d16074f223a33f2d8ccf36c9
Fenando Becerra <fern>
2021-11-02 08:53:06 -0700

    Upper and lower guess limit

--- da1_assignment/day1_assignment/src/main/java/com/ss/week1/day1/day1_assignment/Day1Aassignment2.java
+++ da1_assignment/day1_assignment/src/main/java/com/ss/week1/day1/day1_assignment/Day1Aassignment2.java
@@ -11,17 +11,24 @@ public class Day1Aassignment2 {
     // Function random guess
     public static void randomGuess() {
         // generate random number from 1-100
-        int rand_int2 = ThreadLocalRandom.current().nextInt(1, 6);
+        int randInt = ThreadLocalRandom.current().nextInt(1, 6);
+
+        int upperLimit = randInt + 10;
+        int lowerLimit = randInt - 10;
         // prompt user
-        System.out.println("Range between 1 to 6: " + rand_int2);
+        System.out.println("Range between 1 to 6: " + randInt);
         // scanner object
         Scanner scanObj = new Scanner(System.in);
         // store user integer guess
         int userGuess = scanObj.nextInt();
-        if (userGuess == rand_int2) {
+        if (userGuess == randInt) {
             System.out.println("Congratulations you won!");
 
+        } else if (userGuess > lowerLimit && userGuess < upperLimit) {
+            System.out.println("Upper Limit:" + upperLimit + "\nLowerLimit: " + lowerLimit);
+            System.out.println("Congratulations you won!");
         }
+
         // prompt user
         System.out.println("Guess: " + userGuess);
         // scanner object close
