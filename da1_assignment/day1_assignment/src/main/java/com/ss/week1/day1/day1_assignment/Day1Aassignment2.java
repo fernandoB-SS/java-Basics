b2462c443a9be1ad04a2f69d6e1c0f7a6675d066
Fenando Becerra <fern>
2021-11-02 09:24:58 -0700

    While loop

--- da1_assignment/day1_assignment/src/main/java/com/ss/week1/day1/day1_assignment/Day1Aassignment2.java
+++ da1_assignment/day1_assignment/src/main/java/com/ss/week1/day1/day1_assignment/Day1Aassignment2.java
@@ -12,27 +12,49 @@ public class Day1Aassignment2 {
     public static void randomGuess() {
         // generate random number from 1-100
         int randInt = ThreadLocalRandom.current().nextInt(1, 6);
-
         int upperLimit = randInt + 10;
         int lowerLimit = randInt - 10;
+        int numberOfGuesses = 5;
         // prompt user
         System.out.println("Range between 1 to 6: " + randInt);
         // scanner object
         Scanner scanObj = new Scanner(System.in);
         // store user integer guess
         int userGuess = scanObj.nextInt();
-        if (userGuess == randInt) {
-            System.out.println("Congratulations you won!");
+        boolean gameOver = false;
 
-        } else if (userGuess > lowerLimit && userGuess < upperLimit) {
-            System.out.println("Upper Limit:" + upperLimit + "\nLowerLimit: " + lowerLimit);
-            System.out.println("Congratulations you won!");
+        while (gameOver == false) {
+            if (userGuess == randInt) {
+                System.out.println("Congratulations you won!");
+                gameOver = true;
+            } else if (userGuess > lowerLimit && userGuess < upperLimit) {
+                System.out.println("Upper Limit:" + upperLimit + "\nLowerLimit: " + lowerLimit);
+                System.out.println("Congratulations you won!");
+                gameOver = true;
+            } else if (numberOfGuesses == 0) {
+                System.out.println("Congratulations you won!");
+                gameOver = true;
+
+            } else if (userGuess > lowerLimit && userGuess < upperLimit) {
+                System.out.println("Upper Limit:" + upperLimit + "\nLowerLimit: " + lowerLimit);
+                System.out.println("Congratulations you won!");
+                gameOver = true;
+            } else if (numberOfGuesses == 0) {
+                System.out.println("Game over!");
+                gameOver = true;
+
+            } else {
+                numberOfGuesses--;
+                System.out.println("Number of tries: " + numberOfGuesses);
+                System.out.println("Please try again");
+                // store user integer guess
+                userGuess = scanObj.nextInt();
+            }
         }
 
-        // prompt user
-        System.out.println("Guess: " + userGuess);
         // scanner object close
         scanObj.close();
+
     }
 
     public static void main(String[] args) {
