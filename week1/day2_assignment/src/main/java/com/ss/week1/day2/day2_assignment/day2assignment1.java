a3e8bd9b637dd9b165d7e91d209844b02c8a0eac
Fenando Becerra <fern>
2021-11-03 07:53:50 -0700

    Assignment initial commit

---
+++ week1/day2_assignment/src/main/java/com/ss/week1/day2/day2_assignment/day2assignment1.java
@@ -0,0 +1,16 @@
+package com.ss.week1.day2.day2_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class day2assignment1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day2AssignmentApplication.class, args);
+        System.out.println("Day 2 Assingment 1");
+        for (String s : args) {
+            System.out.println(s);
+        }
+    }
+}
---
+++ week1/day2_assignment/src/main/java/com/ss/week1/day2/day2_assignment/day2assignment2.java
@@ -0,0 +1,13 @@
+package com.ss.week1.day2.day2_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class day2assignment2 {
+    public static void main(String[] args) {
+        SpringApplication.run(Day2AssignmentApplication.class, args);
+        System.out.println("Day 2 Assingment 2");
+
+    }
+}
